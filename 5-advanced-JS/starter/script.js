e293a2552fcbd095ee71b68f0b34e77b87ca3563
XenderMd <denis.stavilamd@gmail.com>
2019-10-29 21:27:15 +0100

    Lecture: Closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -171,7 +171,7 @@ var jane = Object.create(personProto,
 }
 game ();*/
 
-(function () 
+/*(function () 
 {
     var score = Math.random()*10;
     console.log(score >=5);   
@@ -183,7 +183,82 @@ game ();*/
     var score = Math.random()*10;
     console.log(score >=5 - goodluck);   
 }
-) (5);
+) (5);*/
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////// Lectures: Closures
+
+
+/*function retirement (retirementAge)
+{
+    var a = ' years left until retirement. ';
+    return function (yearOfBirth)
+    {
+        var age=2016 - yearOfBirth;
+        console.log((retirementAge-age)+a);
+    }
+}
+
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990); */
+
+
+/* function interviewQuestion(job)
+    {
+        var question;
+        if (job === 'designer')
+        {
+            return function (name)
+            {
+                console.log(name + ', can you please explain what UX design is ?');
+            }
+        }
+        else if (job === 'teacher')
+        {
+            return function (name)
+            {
+                console.log ('What subject do you teach, ' + name + '?');
+            }
+        }
+        else 
+        {
+            return function (name)
+            {
+                console.log ('Hello '+ name + ', what do you do ?');
+            }
+        }
+    }
+*/
+
+function interviewQuestion (job)
+{
+    return function (name)
+    {
+        if (job === 'designer')
+        {
+            console.log(name + ', can you please explain what UX design is ?');
+        }
+        else if (job === 'teacher')
+        {
+            console.log ('What subject do you teach, ' + name + '?');
+        }
+        else
+        {
+            console.log ('Hello '+ name + ', what do you do ?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+
+
 
 
 
