af177d414c8d2a1c7f9fe5d853c10f1c2db9ee47
StavilaD <denis.stavila@prodsp.hu>
2019-11-25 21:32:11 +0100

    Arrow functions 2: Lexical 'this' keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -74,11 +74,12 @@ console.log(n.repeat(5));
 ////////////////////////////////////////////////////////////////////////////////////////////
 //// Lecture Arrow functions
 
-const years = [1990, 1965, 1982, 1937];
+/*const years = [1990, 1965, 1982, 1937];
 
 // ES5
 
-var ages5 = years.map(function (el) {
+var ages5 = years.map(function (el) 
+{
     return 2016 - el;
 } );
 console.log(ages5);
@@ -100,3 +101,86 @@ ages6= years.map((el, index) =>
 } );
 
 console.log(ages6);
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//// Lecture: Arrow functions 2
+
+//ES5
+
+/*var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function ()
+    {
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function()
+        {
+           var str= 'This is box number ' + self.position + ' and it is ' + self.color;
+           alert(str);
+        });   
+    }
+}
+
+box5.clickMe();
+
+*/
+//ES6
+
+/*const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function ()
+    {
+        document.querySelector('.green').addEventListener('click', 
+        ()=>{
+           var str= 'This is box number ' + this.position + ' and it is ' + this.color;
+           alert(str);
+        });   
+    }
+}
+
+box6.clickMe(); */
+
+
+//ES 5
+/*function Person (name)
+{
+    this.name=name;
+}
+
+Person.prototype.myFriends5= function (friends)
+{
+    var arr = friends.map(function(el)
+    {
+        return this.name+' is friends with ' + el;
+    }.bind(this));
+
+    console.log(arr);
+};
+
+var friends = ['Bob', 'John', 'Mark'];
+new Person ('John').myFriends5(friends);
+*/
+
+/*
+//ES 6
+function Person (name)
+{
+    this.name=name;
+}
+
+Person.prototype.myFriends5 = function (friends)
+{
+    var arr = friends.map(el =>
+    {
+        return this.name+' is friends with ' + el;
+    });
+
+    console.log(arr);
+};
+
+var friends = ['Bob', 'John', 'Mark'];
+new Person ('John').myFriends5(friends);
+*/
+
