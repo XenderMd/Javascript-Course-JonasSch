9dd76d060687dbb59af880b6bd5b92e72342cf89
StavilaD <denis.stavila@prodsp.hu>
2019-11-30 15:24:35 +0100

    - Lecture: Default Parameters

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -386,3 +386,35 @@ function isFullAge6(limit,...years) // ... the REST parameters
 isFullAge6(18, 1990, 1999, 1965);
 */
 
+
+
+///////////////////////////////////////////////////////////////
+//////////Lecture: Default Parameters
+
+
+// ES5
+/*
+function SmithPerson (firstName, yearOfBirth, lastName, nationality)
+{
+    lastName===undefined ? lastName ='Smith' : lastName=lastName;
+    nationality===undefined ? nationality='American': nationality=nationality;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality=nationality;
+}
+
+
+var john = new SmithPerson ('John', 1990);
+var emily = new SmithPerson ('Emily', 1983, 'Diaz', 'spanish');
+*/
+
+// ES6 
+function SmithPerson (firstName, yearOfBirth, lastName='Smith', nationality='American')
+{
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality=nationality;
+}
