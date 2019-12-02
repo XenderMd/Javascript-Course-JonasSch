431ad0c2e32a4ec2d468913c388a5edeb486127d
XenderMd <denis.stavilamd@gmail.com>
2019-12-02 21:41:28 +0100

    Lecture: Maps

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -411,10 +411,52 @@ var emily = new SmithPerson ('Emily', 1983, 'Diaz', 'spanish');
 */
 
 // ES6 
-function SmithPerson (firstName, yearOfBirth, lastName='Smith', nationality='American')
+/*function SmithPerson (firstName, yearOfBirth, lastName='Smith', nationality='American')
 {
     this.firstName = firstName;
     this.lastName = lastName;
     this.yearOfBirth = yearOfBirth;
     this.nationality=nationality;
 }
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///// Lecture: Maps
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version ?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3,'ES2015');
+question.set(4,'ES7');
+question.set('answer', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again !');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+if (question.has(4))
+{
+    //question.delete(4);
+    //console.log('Answer 4 is here');
+}
+
+//question.clear();
+
+//question.forEach ((value, key) => console.log(`This is ${key}, abd it's set to ${value}`));
+
+for (let [key, value] of question.entries())  // destructuring
+{
+    if (typeof (key) ==='number')
+    {
+        console.log(`Answer ${key}: it's set to ${value}`);
+    }
+}
+
+const ans = parseInt (prompt('Write the correct answer'));
+
+console.log(question.get(ans ===question.get('answer')));
+
+
