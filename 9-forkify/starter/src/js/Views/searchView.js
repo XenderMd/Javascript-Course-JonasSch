bb23168eedfec44968e17c03d71848b25026fc54
StavilaD <denis.stavila@prodsp.hu>
2019-12-22 12:38:29 +0100

    Lecture: Building the SearchView part 2

--- 9-forkify/starter/src/js/Views/searchView.js
+++ 9-forkify/starter/src/js/Views/searchView.js
@@ -14,6 +14,26 @@ export const clearResults = () =>
     elements.searchResList.innerHTML='';
 };
 
+const limitRecipeTitle = (title, limit=17) =>
+{
+    const newTitle = [];
+    // Pasta with tomato and spinach
+    if (title.length >limit)
+    {
+        title.split(' ').reduce((acc, curr)=>{
+            if(acc+curr.length<=limit)
+            {
+                newTitle.push(curr);
+            }
+            return acc+curr.length;
+        },0);
+
+        return `${newTitle.join(' ')} ...`
+    }
+
+    return title;
+}
+
 const renderRecipe = recipe =>{
     const markup = `
          <li>
@@ -22,7 +42,7 @@ const renderRecipe = recipe =>{
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
